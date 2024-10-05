73d8cf4455450910558d1dfae12fa599b4bde56f
Oka <lehaloot@mail.ru>
2024-10-05 18:39:43 +0300

    new window

--- src/App.js
+++ src/App.js
@@ -6,19 +6,37 @@ import MainTable from "./components/MainTable";
 import { useState } from "react";
 
 function App() {
-    const [selectedTeachers, setSelectedTeachers] = useState([]); // Массив выбранных преподавателей
+    const [selectedTeachers, setSelectedTeachers] = useState([]);
+    const [selectedGroups, setSelectedGroups] = useState([]);// Массив выбранных преподавателей
     const [weekData, setWeekData] = useState([
-        { teacher: "Крамная Е.С.", week: "Пн", time: "08:30-10:00", business: true, auditory: "101" },
-        { teacher: "Шулёва Ю.Н.", week: "Вт", time: "10:10-11:40", business: false, auditory: "102" },
-        { teacher: "Крамная Е.С.", week: "Ср", time: "13:30-15:00", business: true, auditory: "101" }
+        { teacher: "Крамная Е.С.", week: "Пн", time: "08:30-10:00", business: true, auditory: "101", group: "ЦИС-38" },
+        { teacher: "Шулёва Ю.Н.", week: "Вт", time: "10:10-11:40", business: false, auditory: "102",group: "ЦИСБ-34" },
+        { teacher: "Крамная Е.С.", week: "Ср", time: "13:30-15:00", business: true, auditory: "101", group: "ЦИС-38" }
     ]); // Пример данных
+    const [page, setPage] = useState(0);
     
+    console.log(page)
     return (
         <div className="App">
             <div className="container">
                 <div className="main">
-                    <MainList selectedTeachers={selectedTeachers} setSelectedTeachers={setSelectedTeachers} />
+                    
+                    
+                    <MainList selectedTeachers={selectedTeachers} setSelectedTeachers={setSelectedTeachers} selectedGroups={selectedGroups} setSelectedGroups={setSelectedGroups} page={page}/>
                     <MainTable selectedTeachers={selectedTeachers} weekData={weekData} />
+                </div>
+                <div className="main__btns">
+                    <button
+                        onClick={() => {
+                            setPage(page - 1);
+                        }}
+                    >-</button>
+                    <button
+                        onClick={() => {
+                            setPage(page + 1);
+                        }}
+                    >+</button>
+                    
                 </div>
             </div>
         </div>
--- src/components/MainList.jsx
+++ src/components/MainList.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const MainList = ({ selectedTeachers, setSelectedTeachers }) => {
+const MainList = ({ selectedTeachers, setSelectedTeachers, page, selectedGroups, setSelectedGroups }) => {
     const teachers = ["Крамная Е.С.", "Шулёва Ю.Н."]; // Данные о преподавателях
+    const groups = ["ЦИС-38", "ЦИСБ-34"]; // Данные о преподавателях
     
-    const handleCheckboxChange = (teacher) => {
+    const handleCheckboxChangeTeachers = (teacher) => {
         if (selectedTeachers.includes(teacher)) {
             // Убираем из списка если уже выбран
             setSelectedTeachers(selectedTeachers.filter((t) => t !== teacher));
@@ -12,29 +13,65 @@ const MainList = ({ selectedTeachers, setSelectedTeachers }) => {
             setSelectedTeachers([...selectedTeachers, teacher]);
         }
     };
+    const handleCheckboxChangeGroups = (group) => {
+        if (selectedGroups.includes(group)) {
+            // Убираем из списка если уже выбран
+            setSelectedGroups(selectedGroups.filter((t) => t !== group));
+        } else {
+            // Добавляем преподавателя в список
+            setSelectedGroups([...selectedGroups, group]);
+        }
+    };
     
-    return (
-        <div>
-            <div className="wrapper__card">
-                <div className="card">
-                    <h2 className="card__name">Преподаватели</h2>
-                    <div className="card__body">
-                        <input type="text" className="card__search" placeholder="Найти преподавателя" />
-                        {teachers.map((teacher) => (
-                            <div key={teacher}>
-                                <input
-                                    type="checkbox"
-                                    checked={selectedTeachers.includes(teacher)}
-                                    onChange={() => handleCheckboxChange(teacher)}
-                                />
-                                <p>{teacher}</p>
-                            </div>
-                        ))}
+    if (page == 0){
+        return (
+            <div>
+                <div className="wrapper__card">
+                    <div className="card">
+                        <h2 className="card__name">Преподаватели</h2>
+                        <div className="card__body">
+                            <input type="text" className="card__search" placeholder="Найти преподавателя" />
+                            {teachers.map((teacher) => (
+                                <div key={teacher}>
+                                    <input
+                                        type="checkbox"
+                                        checked={selectedTeachers.includes(teacher)}
+                                        onChange={() => handleCheckboxChangeTeachers(teacher)}
+                                    />
+                                    <p>{teacher}</p>
+                                </div>
+                            ))}
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
+    if (page == 1){
+        return (
+            <div>
+                <div className="wrapper__card">
+                    <div className="card">
+                        <h2 className="card__name">Группы</h2>
+                        <div className="card__body">
+                            <input type="text" className="card__search" placeholder="Найти группу" />
+                            {groups.map((group) => (
+                                <div key={group}>
+                                    <input
+                                        type="checkbox"
+                                        checked={selectedGroups.includes(group)}
+                                        onChange={() => handleCheckboxChangeGroups(group)}
+                                    />
+                                    <p>{group}</p>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    
 };
 
 export default MainList;
